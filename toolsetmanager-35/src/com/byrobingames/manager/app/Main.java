8f8af4db461a0e8347dbf4eac8fbdabed0440127
Justin Espedal <justin.espedal@gmail.com>
2020-08-07 04:13:54 +0900

    Add capabilities for resolving dependencies for engine extensions

--- toolsetmanager-35/src/com/byrobingames/manager/app/Main.java
+++ toolsetmanager-35/src/com/byrobingames/manager/app/Main.java
@@ -1,11 +1,17 @@
 package com.byrobingames.manager.app;
 
+import static stencyl.sw.util.Locations.getPath;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
 
 import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
@@ -13,16 +19,18 @@ import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
-import javax.swing.text.JTextComponent;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.mutable.MutableBoolean;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.log4j.Logger;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
-import com.byrobingames.manager.ByRobinGameExtension;
-import com.byrobingames.manager.app.StatusBar;
 import com.byrobingames.manager.app.pages.AppLovinPage;
 import com.byrobingames.manager.app.pages.FacebookPage;
 import com.byrobingames.manager.app.pages.GeneralPage;
@@ -31,7 +39,6 @@ import com.byrobingames.manager.app.pages.LeadboltPage;
 import com.byrobingames.manager.app.pages.LocalNotifPage;
 import com.byrobingames.manager.app.pages.ReplayKitPage;
 import com.byrobingames.manager.app.pages.SimpleSharePage;
-import com.byrobingames.manager.app.pages.SourcePage;
 import com.byrobingames.manager.app.pages.StartAppPage;
 import com.byrobingames.manager.app.pages.TapdaqPage;
 import com.byrobingames.manager.app.pages.UnityAdsPage;
@@ -40,10 +47,23 @@ import com.byrobingames.manager.app.pages.WebViewPage;
 import com.byrobingames.manager.res.Resources;
 import com.byrobingames.manager.writer.FileWriter;
 
+import pulpcore.net.Download;
 import stencyl.core.lib.Game;
-
+import stencyl.sw.SW;
+import stencyl.sw.app.tasks.buildgame.GameBuilder;
+import stencyl.sw.editors.game.advanced.EngineExtensionManager;
+import stencyl.sw.editors.game.advanced.ExtensionInstance;
 import stencyl.sw.lnf.Theme;
-import stencyl.sw.util.dg.DialogPanel;
+import stencyl.sw.loc.LanguagePack;
+import stencyl.sw.prefs.runconfigs.BuildConfig;
+import stencyl.sw.util.FileHelper;
+import stencyl.sw.util.Locations;
+import stencyl.sw.util.ProgressBar;
+import stencyl.sw.util.ProgressDialog;
+import stencyl.sw.util.ProgressTracker;
+import stencyl.sw.util.Util;
+import stencyl.sw.util.dg.MessageDialog;
+import stencyl.sw.util.platform.haxe.Haxe;
 
 public class Main extends JPanel
 {
@@ -337,13 +357,359 @@ public class Main extends JPanel
 		currentPage.repaint();
 	}
 	
+	//XXX: This should be easily available through extension API
+	private static GameBuilder getRunningBuilder()
+	{
+		Optional<SwingWorker<Integer, Void>> builderWorker =
+			SW.get().getTaskManager().getRunningTask
+			(
+				task -> task.getClass().getEnclosingClass() == GameBuilder.class
+			);
+		
+		if(builderWorker.isEmpty())
+		{
+			log.info("No running GameBuilder.");
+			return null;
+		}
+		
+		Object o;
+		try
+		{
+			o = FieldUtils.readDeclaredField(builderWorker.get(), "this$0", true);
+		}
+		catch (IllegalAccessException e)
+		{
+			log.error(e.getMessage(), e);
+			return null;
+		}
+		
+		if(o instanceof GameBuilder)
+		{
+			return (GameBuilder) o;
+		}
+		
+		log.error("Couldn't access GameBuilder object.");
+		return null;
+	}
+	
+	//XXX: This should be easily available through extension API
+	private static BuildConfig getRunningBuildConfig()
+	{
+		GameBuilder builder = getRunningBuilder();
+		
+		if(builder == null) return null;
+		
+		BuildConfig buildConfig;
+		try
+		{
+			buildConfig = (BuildConfig) FieldUtils.readField(builder, "buildConfig", true);
+		}
+		catch (IllegalAccessException e)
+		{
+			log.error(e.getMessage(), e);
+			return null;
+		}
+		
+		return buildConfig;
+	}
+	
 	public void writeBuildFiles() throws IOException
 	{
+		BuildConfig buildConfig = getRunningBuildConfig();
+		
+		for(ExtensionInstance inst : Game.getGame().getExtensions().values())
+		{
+			if(inst.isEnabled())
+			{
+				File extensionRoot = new File(Locations.getGameExtensionLocation(inst.getExtensionID()));
+				File dependencies = new File(extensionRoot, "brg-dependencies.xml");
+				if(dependencies.exists())
+				{
+					Element deps = FileHelper.readXMLFromFile(dependencies).getDocumentElement();
+					resolveDependencies(deps.getChildNodes(), inst.getExtensionID(), buildConfig);
+				}
+			}
+		}
+		
 		new FileWriter(Game.getGame()).writeByRobinAssets();
 		new FileWriter(Game.getGame()).writeAppLovinIncludeFile();
 		new FileWriter(Game.getGame()).writeTapdaqIncludeFile();
 	}
 	
+	private void resolveDependencies(NodeList nl, String extensionID, BuildConfig buildConfig)
+	{
+		for(int i = 0; i < nl.getLength(); ++i)
+		{
+			if(nl.item(i) instanceof Element)
+			{
+				resolveDependencies((Element) nl.item(i), extensionID, buildConfig);
+			}
+		}
+	}
 	
-}
+	private void resolveDependencies(Element e, String extensionID, BuildConfig buildConfig)
+	{
+		if(e.hasAttribute("if"))
+		{
+			String condition = e.getAttribute("if");
+			if(!buildConfig.platform.getBuildString().equals(condition))
+				return;
+		}
+		
+		switch(e.getTagName())
+		{
+			case "section":
+				resolveDependencies(e.getChildNodes(), extensionID, buildConfig);
+				break;
+			case "framework":
+				resolveFramework(e, extensionID);
+				break;
+			case "ndll":
+				resolveNDLL(e, extensionID, buildConfig);
+				break;
+			default:
+				log.error("Unkown dependency type: " + e.getTagName());
+		}
+	}
+	
+	/*
+	 * Example:
+	 * 
+	 * <framework
+     *      name="UnityAds.framework"
+     *      version="3.2.0"
+     *      source="https://github.com/Unity-Technologies/unity-ads-ios/releases/download/3.2.0/UnityAds.framework.zip" />
+	 */
+	private void resolveFramework(Element e, String extensionID)
+	{
+		String name = e.getAttribute("name");
+		String version = e.getAttribute("version");
+		String source = e.getAttribute("source");
+		
+		//stencylworks/brg-frameworks
+		File frameworksCache = new File(Locations.getWorkspaceRoot(), "brg-frameworks");
+		
+		//stencylworks/brg-frameworks/UnityAds.framework/3.2.0/UnityAds.framework
+		File namedFrameworkCache = new File(frameworksCache, getPath(name, version, name));
+		if(!namedFrameworkCache.exists())
+		{
+			//UnityAds.framework.zip
+			String downloadFilename = source.substring(source.lastIndexOf("/") + 1);
+			//stencylworks/brg-frameworks/.cache/UnityAds.framework/3.2.0/UnityAds.framework.zip
+			File downloadDestination = new File(frameworksCache, getPath(".cache", name, version, downloadFilename));
+			
+			if(!downloadDestination.exists())
+			{
+				downloadFile(source, name, "Error downloading " + name, downloadDestination, LanguagePack.get());
+			}
+			
+			//saved .zip -> stencylworks/brg-frameworks/UnityAds.framework/3.2.0
+			namedFrameworkCache.getParentFile().mkdirs();
+			FileHelper.unzip(downloadDestination, namedFrameworkCache.getParentFile());
+		}
+		
+		File namedFramework = new File(Locations.getGameExtensionLocation(extensionID), getPath("frameworks", name));
+		
+		Path link = namedFramework.toPath();
+		Path linkTo = namedFrameworkCache.toPath();
+		Path linkedPath = null;
+		if(Files.isSymbolicLink(link))
+		{
+			try
+			{
+				linkedPath = Files.readSymbolicLink(link);
+			}
+			catch (IOException ex)
+			{
+				log.error(ex.getMessage(), ex);
+			}
+		}
+		
+		try
+		{
+			if(linkedPath == null || !Files.isSameFile(linkedPath, linkTo))
+			{
+				if(namedFramework.exists())
+				{
+					namedFramework.delete();
+				}
+				try
+				{
+					Files.createSymbolicLink(link, linkTo);
+				}
+				catch (UnsupportedOperationException ex)
+				{
+					log.error(ex.getMessage(), ex);
+				}
+			}
+		}
+		catch (IOException ex)
+		{
+			log.error(ex.getMessage(), ex);
+		}
+	}
+	
+	/*
+	 * Example:
+	 * 
+	 * <ndll buildfile="project/build" output="project/ndll/iPhone" />
+	 */
+	private void resolveNDLL(Element e, String extensionID, BuildConfig buildConfig)
+	{
+		File extensionFolder = new File(Locations.getGameExtensionLocation(extensionID));
+		File buildFile = new File(extensionFolder, e.getAttribute("buildfile"));
+		File output = new File(extensionFolder, e.getAttribute("output"));
+		
+		if(!output.exists())
+		{
+			Util.buildCommand(buildFile.getAbsolutePath())
+				.environment(Haxe.getStencylEnvironment())
+				.workingDir(buildFile.getParentFile())
+				.runAndWait();
+		}
+	}
+	
+	//XXX: this is based on FileHelper.downloadFile for the purpose of allowing us to do this in a blocking manner.
+	public static void downloadFile(final String URL, final String name, final String errorMessage, final File destination, final LanguagePack lang)
+	{
+		downloadFile(URL, name, errorMessage, destination, lang, new ProgressDialog(SW.get(), lang.get("downloader.title", new String[] { name })));
+	}
+	
+	public static void downloadFile(final String URL, final String name, final String errorMessage, final File destination, final LanguagePack lang, final ProgressTracker dg)
+	{
+		log.debug(URL);
 
+		if(dg != null)
+			dg.setVisible(true);
+
+		SwingWorker<Integer, Void> worker = new SwingWorker<Integer, Void>()
+		{
+			int pct = 0;
+
+			@Override
+			public Integer doInBackground()
+			{
+				final Download d = Download.startDownload(URL);
+				pct = 0;
+
+				if(dg != null)
+					dg.repaint();
+
+				try
+				{
+					Thread.sleep(50);
+				}
+
+				catch (InterruptedException e)
+				{
+					log.error(e.getMessage(), e);
+				}
+
+				Thread.yield();
+
+				SwingUtilities.invokeLater(new Runnable()
+				{
+					@Override
+					public void run()
+					{
+						if(dg != null)
+							dg.updateFile(1, 1, name);
+					}
+				});
+
+				while (d.getState() != Download.SUCCESS)
+				{
+					int newpct = (int) Math.floor(ProgressBar.PRECISION * d.getPercentDownloaded());
+
+					if (newpct > pct)
+					{
+						SwingUtilities.invokeLater(new Runnable()
+						{
+							@Override
+							public void run()
+							{
+								if(dg != null)
+								{
+									dg.updateFileSize(d.getBytesRead() / 1024, d.getSize() / 1024, d.getEstimatedTimeRemaining());
+
+									dg.update(ProgressBar.PRECISION * d.getPercentDownloaded());
+								}
+							}
+						});
+					}
+
+					pct = newpct;
+
+					if (d.getState() == Download.ERROR)
+					{
+						//Throw an error dialog.
+						log.error("Download failed.");
+						if(dg != null)
+							dg.setVisible(false);
+						cancel(true);
+
+						SwingUtilities.invokeLater(new Runnable()
+						{
+							@Override
+							public void run()
+							{
+								MessageDialog.showErrorDialog(lang.get("globals.error"), errorMessage);
+							}
+						});
+
+						return -1;
+					}
+
+					try
+					{
+						Thread.sleep(100);
+					}
+
+					catch (InterruptedException e)
+					{
+						log.error(e.getMessage(), e);
+						cancel(true);
+					}
+				}
+
+				try
+				{
+					FileHelper.delete(destination);
+					destination.getParentFile().mkdirs();
+					log.debug("Saving downloaded file to: " + destination);
+					FileUtils.writeByteArrayToFile(destination, d.getData());
+				}
+
+				catch (IOException e)
+				{
+					log.error(e.getMessage(), e);
+					cancel(true);
+				}
+
+				return 0;
+			}
+
+			@Override
+			public void done()
+			{
+				super.done();
+				if(dg != null)
+					dg.dispose();
+			}
+		};
+
+		worker.execute();
+		
+		while(!worker.isDone())
+		{
+			try
+			{
+				Thread.sleep(50);
+			}
+			catch (InterruptedException e)
+			{
+				log.error(e.getMessage(), e);
+			}
+		}
+	}
+}
